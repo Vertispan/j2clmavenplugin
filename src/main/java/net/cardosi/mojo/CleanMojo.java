24eaed8aec2c89f2daf027d9e5f14f6d4921f3b8
Colin Alworth <colin@colinalworth.com>
2019-06-11 10:21:39 -0500

    Make the clean mojo only clean up the current project's cache by default

--- src/main/java/net/cardosi/mojo/CleanMojo.java
+++ src/main/java/net/cardosi/mojo/CleanMojo.java
@@ -15,30 +15,69 @@
  */
 package net.cardosi.mojo;
 
+import net.cardosi.mojo.cache.DiskCache;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.descriptor.PluginDescriptor;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
-import java.nio.file.Path;
+import java.nio.file.*;
+import java.util.List;
 
 @Mojo(name = "clean")
 public class CleanMojo extends AbstractGwt3BuildMojo {
 
+    @Parameter(property = "artifact")
+    protected String artifact;
+
+    @Parameter(defaultValue = "${reactorProjects}", required = true, readonly = true)
+    protected List<MavenProject> reactorProjects;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+        PluginDescriptor pluginDescriptor = (PluginDescriptor) getPluginContext().get("pluginDescriptor");
+        String pluginVersion = pluginDescriptor.getVersion();
+
+        Path currentPluginCacheDir = Paths.get(gwt3BuildCacheDir.getAbsolutePath(), pluginVersion);
+
         try {
-            getLog().info("Deleting GWT3 build cache");
-            recursivelyDeleteDir(gwt3BuildCacheDir.toPath());
-        } catch (Throwable t) {
+            if (artifact == null) {
+                // delete all cache entries from the current reactor
+                for (MavenProject reactorProject : reactorProjects) {
+                    getLog().info("Deleting cache for " + reactorProject);
+                    deleteArtifact(currentPluginCacheDir, reactorProject.getArtifactId());
+                }
+
+            } else if (artifact.equals("*")) {
+                // delete all cache entries, regardless of source
+                getLog().info("Deleting entire GWT3 build cache");
+                recursivelyDeleteDir(gwt3BuildCacheDir.toPath());
+            } else {
+                // identify the cache entry we were ask to remove and delete all items found which might
+                // match that
+                    getLog().info("Deleting cache for " + artifact);
+                deleteArtifact(currentPluginCacheDir, artifact);
+            }
+        } catch (IOException t) {
             throw new MojoFailureException(t.getMessage(), t);
         }
     }
 
-    private void recursivelyDeleteDir(Path path) throws IOException {
+    private void deleteArtifact(Path baseDir, String artifact) throws IOException {
+        try (DirectoryStream<Path> entries = Files.newDirectoryStream(baseDir)) {
+            for (Path entry : entries) {
+                if (entry.getFileName().toString().startsWith(artifact + "-")) {
+                    getLog().info("Deleting directory " + entry);
+                    recursivelyDeleteDir(entry);
+                }
+            }
+        }
+    }
+
+    private static void recursivelyDeleteDir(Path path) throws IOException {
         if (Files.exists(path)) {
             if (Files.isDirectory(path, LinkOption.NOFOLLOW_LINKS)) {
                 try (DirectoryStream<Path> entries = Files.newDirectoryStream(path)) {
