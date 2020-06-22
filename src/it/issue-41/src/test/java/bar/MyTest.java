04a2573d7451d0ee89c889d5fe785dbf01d0eda4
Colin Alworth <colin@colinalworth.com>
2020-06-21 20:32:32 -0500

    Test sample for issue #41, can't seem to reproduce
    Closes #41

---
+++ src/it/issue-41/src/test/java/bar/MyTest.java
@@ -0,0 +1,12 @@
+package bar;
+
+import com.google.j2cl.junit.apt.J2clTestInput;
+
+import org.junit.Test;
+
+@J2clTestInput(MyTest.class)
+public class MyTest {
+    @Test
+    public void testMethod() {
+    }
+}
---
+++ src/it/issue-41/src/test/java/foo/MyTest.java
@@ -0,0 +1,12 @@
+package foo;
+
+import com.google.j2cl.junit.apt.J2clTestInput;
+
+import org.junit.Test;
+
+@J2clTestInput(MyTest.class)
+public class MyTest {
+    @Test
+    public void testMethod() {
+    }
+}
