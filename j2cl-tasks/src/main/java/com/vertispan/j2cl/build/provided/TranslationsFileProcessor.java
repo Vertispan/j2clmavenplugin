0827a8f192fd9472700721bfeba28ddecdaeabdb
Dmitii Tikhomirov <chani.liet@gmail.com>
2022-06-25 23:50:04 -0700

    generate resulted xtb file made of parent locales and all other suitable xtb files, provide it to closure

--- j2cl-tasks/src/main/java/com/vertispan/j2cl/build/provided/TranslationsFileProcessor.java
+++ j2cl-tasks/src/main/java/com/vertispan/j2cl/build/provided/TranslationsFileProcessor.java
@@ -1,10 +1,9 @@
 package com.vertispan.j2cl.build.provided;
 
-import com.vertispan.j2cl.build.task.BuildLog;
-import com.vertispan.j2cl.build.task.CachedPath;
-import com.vertispan.j2cl.build.task.Config;
-import com.vertispan.j2cl.build.task.Input;
+import com.vertispan.j2cl.build.task.*;
+import org.apache.commons.io.FileUtils;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -13,10 +12,9 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public interface TranslationsFileProcessor {
@@ -35,7 +33,7 @@ public interface TranslationsFileProcessor {
         return new TranslationsFileNotDefined(false);
     }
 
-    Optional<File> getTranslationsFile(List<Input> inputs, BuildLog log);
+    Optional<File> getTranslationsFile(List<Input> inputs, TaskContext context);
 
     class TranslationsFileNotDefined implements TranslationsFileProcessor {
         private final boolean shouldWarn;
@@ -45,9 +43,9 @@ public interface TranslationsFileProcessor {
         }
 
         @Override
-        public Optional<File> getTranslationsFile(List<Input> inputs, BuildLog log) {
+        public Optional<File> getTranslationsFile(List<Input> inputs, TaskContext context) {
             if (shouldWarn) {
-                log.warn("translationsFile only works in the ADVANCED optimization level, in other levels the default messages values will be used");
+                context.log().warn("translationsFile only works in the ADVANCED optimization level, in other levels the default messages values will be used");
             }
 
             return Optional.empty();
@@ -62,7 +60,7 @@ public interface TranslationsFileProcessor {
         }
 
         @Override
-        public Optional<File> getTranslationsFile(List<Input> inputs, BuildLog log) {
+        public Optional<File> getTranslationsFile(List<Input> inputs, TaskContext context) {
             List<File> temp = inputs.stream()
                     .map(Input::getFilesAndHashes)
                     .flatMap(Collection::stream)
@@ -71,7 +69,7 @@ public interface TranslationsFileProcessor {
                     .collect(Collectors.toList());
 
             if (temp.isEmpty()) {
-                log.warn("no .xtb files was found");
+                context.log().warn("no .xtb files was found");
             }
 
             try {
@@ -86,6 +84,9 @@ public interface TranslationsFileProcessor {
 
                 DocumentBuilder db = dbf.newDocumentBuilder();
 
+                HashMap<String, Set<NodeList>> suitableFiles = new HashMap<>();
+                Set<String> locales = locales(locale);
+
                 for (File xtb : temp) {
                     Document doc = db.parse(xtb);
                     doc.getDocumentElement().normalize();
@@ -97,17 +98,109 @@ public interface TranslationsFileProcessor {
 
                     String lang = translationbundleNode.item(0).getAttributes().getNamedItem("lang").getNodeValue();
 
-                    if (locale.equals(lang)) {
-                        return Optional.of(xtb);
+                    if (locales.contains(lang)) {
+                        if(!suitableFiles.containsKey(lang)) {
+                            suitableFiles.put(lang, new HashSet<>());
+                        }
+
+                        suitableFiles.get(lang).add(translationbundleNode);
                     }
                 }
+                if(!suitableFiles.isEmpty()) {
+                    return mergeFiles(suitableFiles, locales, context);
+                }
             } catch (ParserConfigurationException | SAXException | IOException e) {
-                log.error("Error while reading xtb files ", e);
+                context.log().error("Error while reading xtb files ", e);
                 throw new RuntimeException(e);
             }
-            log.warn("No matching locales for " + locale);
+            context.log().warn("No matching locales for " + locale);
             return Optional.empty();
         }
 
+
+        private Set<String> locales(String locale) {
+            Set<String> result = new HashSet<>();
+            result.add(locale);
+
+            if (locale.contains("_")) {
+                // according to spec, locale discaration can have 3 parts max
+                if(locale.indexOf("_") != locale.lastIndexOf("_")) {
+                    result.add(locale.substring(0, locale.lastIndexOf("_")));
+                }
+                result.add(locale.substring(0, locale.indexOf("_")));
+            }
+            return result;
+        }
+
+        private Optional<File> mergeFiles(HashMap<String, Set<NodeList>> suitableFiles, Set<String> locales, TaskContext context) {
+            Map<String, Node> resultedCodeSet = new HashMap<>();
+            // parent nodes first
+            locales.stream().sorted((o1, o2) -> {
+                if(o1.split("_").length > o2.split("_").length) {
+                    return 1;
+                }
+                return -1;
+            }).forEach(locale -> {
+                suitableFiles.get(locale).forEach(node -> {
+                    NodeList children = node.item(0).getChildNodes();
+
+                    for (int i = 0; i < children.getLength(); i++) {
+                        if (children.item(i).getNodeName().equals("translation")) {
+                            String key = children.item(i).getAttributes().getNamedItem("id").getNodeValue();
+                            // override parent keys if already exists
+                            resultedCodeSet.put(key, children.item(i));
+                        }
+                    }
+                });
+            });
+
+            Path folder = context.outputPath();
+            File generated = folder.resolve("generated_messages.xtb").toFile();
+            generateAndWriteXTB(resultedCodeSet, generated);
+            return Optional.of(generated);
+        }
+
+        private void generateAndWriteXTB(Map<String, Node> resultedCodeSet, File generated) {
+            StringBuffer sb = new StringBuffer();
+
+            sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+            sb.append(System.lineSeparator());
+            sb.append("<!DOCTYPE translationbundle SYSTEM \"translationbundle.dtd\">");
+            sb.append(System.lineSeparator());
+
+            sb.append("<translationbundle lang=\"" + locale + "\">");
+            sb.append(System.lineSeparator());
+
+            resultedCodeSet.values().forEach(node -> {
+                sb.append("  <translation id=\"");
+                sb.append(node.getAttributes().getNamedItem("id").getNodeValue());
+                sb.append("\" key=\"");
+                sb.append(node.getAttributes().getNamedItem("key").getNodeValue());
+                sb.append("\">");
+
+                // we have to process placeholders here
+                for (int i = 0; i < node.getChildNodes().getLength(); i++) {
+                    Node innerTextNode = node.getChildNodes().item(i);
+                    if(innerTextNode.getNodeType() == Node.TEXT_NODE) {
+                        sb.append(innerTextNode.getTextContent());
+                    } else if(innerTextNode.getNodeType() == Node.ELEMENT_NODE && innerTextNode.getNodeName().equals("ph")) {
+                        sb.append("<ph name=\"");
+                        sb.append(innerTextNode.getAttributes().getNamedItem("name").getNodeValue());
+                        sb.append("\"/>");
+                    }
+                }
+                sb.append("</translation>");
+                sb.append(System.lineSeparator());
+            });
+
+            sb.append("</translationbundle>");
+            sb.append(System.lineSeparator());
+
+            try {
+                FileUtils.writeStringToFile(generated, sb.toString(), Charset.forName("UTF-8"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
