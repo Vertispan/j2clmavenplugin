8fa9eef1caf75b8415b17c50e3f471549b3ec2f3
Zbynek Konecny <zbynek1729@gmail.com>
2022-12-26 18:26:04 +0100

    Add integration test for generated code in reactor (JavaCC)

--- j2cl-maven-plugin/src/it/annotation-processor-in-reactor/app/src/main/java/com/example/MyApp.java
+++ j2cl-maven-plugin/src/it/annotation-processor-in-reactor/app/src/main/java/com/example/MyApp.java
@@ -3,5 +3,10 @@ package com.example;
 public class MyApp {
     public static void start() {
         MyResources.INSTANCE.resourceInRoot();
+        try {
+            BasicParser.parse("{{}}");
+        } catch(Exception ex) {
+            // parse exception ignored
+        }
     }
 }
